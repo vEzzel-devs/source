87fa26f1609d3fb8e107bd0d616473cb27d22cbe
YISR-UOH <yerko.sepulveda@pregrado.uoh.cl>
2022-09-25 03:28:14 -0400

    Update AutocompleteContainer.jsx

--- src/editor/components/AutocompleteContainer.jsx
+++ src/editor/components/AutocompleteContainer.jsx
@@ -1,15 +1,15 @@
 import { useContext, useState } from "react";
 import { ThemeContext } from "../../context/ThemeContext";
 
-function TooltipCell(props) {
+function AutocompleteContainer(props) {
     const { theme } = useContext(ThemeContext);
     const [status, setStatus] = useState(false);
 
     return (
-        <div className="relative max-w-[12rem] min-w-[12rem] flex flex-col" onClick={() => setStatus(true)} onBlur={() => setStatus(false)}>
+        <div className="relative max-w-[8rem] min-w-[8rem] flex flex-col" onClick={() => setStatus(true)} onBlur={() => setStatus(false)}>
             {props.children}
-            {status && (props.cellRef.current.value != "") && <div role="tooltip" className={"z-20 w-24 absolute left-auto overflow-y-scroll" + theme.scrollbar + theme.mainBg + theme.lightText}>
-                <div className="">
+            {status && (props.cellRef.current.value != "") && <div role="tooltip" className={"z-20 w-36 absolute left-auto overflow-y-scroll" + theme.scrollbar + theme.sidebarBg + theme.lightText}>
+                <div className={theme.hoverSidebar}>
                     {}
                 </div>
             </div>}
@@ -17,4 +17,4 @@ function TooltipCell(props) {
     );
 }
 
-export default TooltipCell
+export default AutocompleteContainer
