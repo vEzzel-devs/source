7eb92f6bd3d9a75c758b092c4c13cb54d45a680e
Vicente-G <vicentea.gonzalez@pregrado.uoh.cl>
2022-10-03 15:54:42 -0300

    hotfix

--- src/components/ContainerBox.jsx
+++ src/components/ContainerBox.jsx
@@ -5,7 +5,7 @@ function ContainerBox(props) {
     const { theme } = useContext(ThemeContext);
     return (
         <div className="md:w-2/3 m-auto max-h-screen h-screen flex flex-col">
-          <div className={"my-auto py-16 md:py-28 justify-center items-center rounded-lg" + theme.mainBg}>
+          <div className={"my-auto py-12 md:py-20 justify-center items-center rounded-lg" + theme.mainBg}>
             { props.children }
           </div>
         </div>
--- src/inicio/sections/About.jsx
+++ src/inicio/sections/About.jsx
@@ -9,13 +9,13 @@ function About() {
 
   return (
     <section className={theme.mainBg} id="about">
-      <div className={"w-full h-screen flex flex-col justify-center items-center p-4" + theme.secondaryBg}>
+      <div className={"w-full h-screen flex flex-col justify-center items-center p-2 md:p-4" + theme.secondaryBg}>
         <ContainerBox>
-          <div className="p-6 flex justify-center items-center">
+          <div className="p-2 md:px-6 md:py-4 flex justify-center items-center">
             <h1 className={"text-4xl text-center" + theme.mainText}>El equipo <Vezzel/></h1>
           </div>
           <br/>
-          <div className="flex md:flex-row flex-col items-center justify-center px-2 md:px-16 md:space-x-4 space-y-2">
+          <div className="flex lg:flex-row flex-col items-center justify-center px-2 lg:px-16 lg:space-x-4 space-y-2">
             <Card name="Vicente González" text="Desarrollador full stack."/>
             <Card name="Hernán Moreno" text="Desarrollador full stack."/>
             <Card name="Yerko Sepúlveda" text="Desarrollador full stack."/>
