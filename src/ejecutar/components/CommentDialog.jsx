5a6a493e609101ceadba31b6cb2a101834ebb7f0
Vicente-G <vicentea.gonzalez@pregrado.uoh.cl>
2022-11-05 19:48:59 -0300

    Comment component and dialog

--- src/ejecutar/components/CommentDialog.jsx
+++ src/ejecutar/components/CommentDialog.jsx
@@ -1,15 +1,15 @@
 import { DialogContent, DialogTitle, Tooltip } from "@mui/material";
 import { Dialog, DialogActions, Zoom } from "@mui/material";
-import { useContext, useState, useRef } from "react";
+import { useContext, useState, useRef, useEffect } from "react";
 import { ThemeContext } from "../../context/ThemeContext";
 import CommentIcon from '@mui/icons-material/Comment';
 import ValueStars from "./ValueStars";
-import { SpreadSheetContext } from "../../context/SpreadSheetContext";
 
 function CommentDialog() {
     const { theme } = useContext(ThemeContext);
     const [ open, setOpen ] = useState(false);
     const [ stars, setStars ] = useState(0);
+    const [ op, setOp ] = useState("Publicar")
     const descRef = useRef();
 
     const handleOpen = () => {
@@ -27,7 +27,7 @@ function CommentDialog() {
 
     const handleSubmit = async () => {
         let desc = descRef.current.value;
-        let res = await savespread(content, title, desc, tags);
+        // query
         setOpen(false);
     };
 
@@ -38,6 +38,22 @@ function CommentDialog() {
         setTags([...newValue]);
     });
 
+    const updateOp = async () => {
+        await new Promise(r => setTimeout(r, 10));
+        let desc = descRef?.current?.value;
+        if (true && stars === 0 && desc === "") {
+            setOp("Eliminar");
+        } else if (true) {
+            setOp("Editar");
+        } else {
+            setOp("Publicar");
+        }
+    }
+
+    useEffect(() => {
+        updateOp();
+    }, [open, stars]);
+
     return (
         <>
             <Tooltip TransitionComponent={Zoom} placement="bottom" enterDelay={500} title={"Valorar"} arrow>
@@ -47,11 +63,11 @@ function CommentDialog() {
                 <DialogTitle>Publica tu opinión!</DialogTitle>
                 <DialogContent>
                     <ValueStars stars={stars} setStars={setStars}/>
-                    <textarea id='desc' ref={descRef} className={"font-mono p-2 mb-2 h-32 w-full outline-0 border-2 overflow-y-scroll resize-none" + theme.mainBg + theme.mainText + theme.borderNavbar + theme.scrollbar} placeholder="Redacta una reseña" style={{fontFamily: "Cascadia Code"}}/>
+                    <textarea id='desc' ref={descRef} className={"font-mono p-2 mb-2 h-32 w-full md:w-96 outline-0 border-2 overflow-y-scroll resize-none" + theme.mainBg + theme.mainText + theme.borderNavbar + theme.scrollbar} placeholder="Redacta una reseña" onChange={updateOp} style={{fontFamily: "Cascadia Code"}}/>
                 </DialogContent>
                 <DialogActions>
                     <button className={"font-mono md:w-1/4 mr-2 p-2 rounded-lg border-2" + theme.mainText + theme.mainBg + theme.secondaryButton} onClick={handleCancel} style={{fontFamily: "Cascadia Code"}}>Cancelar</button>
-                    <button className={"font-mono md:w-1/4 mr-2 p-2 rounded-lg border-2" + theme.mainText + theme.mainBg + theme.primaryButton} type="submit" onClick={handleSubmit} style={{fontFamily: "Cascadia Code"}}>{(stars === 0 && descRef.current.value === "") ? "Eliminar" : "Publicar"}</button>
+                    <button className={"font-mono md:w-1/4 mr-2 p-2 rounded-lg border-2" + theme.mainText + theme.mainBg + theme.primaryButton} type="submit" onClick={handleSubmit} style={{fontFamily: "Cascadia Code"}}>{op}</button>
                 </DialogActions>
             </Dialog>
         </>
--- src/ejecutar/sections/AppContainer.jsx
+++ src/ejecutar/sections/AppContainer.jsx
@@ -1,20 +1,25 @@
 import { ThemeContext } from '../../context/ThemeContext'
 import { UserAppContext } from '../context/UserAppContext'
 import { useContext } from 'react'
+import CommentCard from '../../components/CommentCard';
 
 function AppContainer() {
   const { theme } = useContext(ThemeContext);
   const { userApp } = useContext(UserAppContext);
 
-  /*
-  {userApp.map((row) => {
-    return (
-      <div className="w-full py-2 flex flex-row">
-        { row }
-      </div>
-    );
-  })}
-  */
+  // query de carga de comentarios, y posterior loop sobre el div de comentarios
+  const ph = [
+    {title: "User1", desc: "Lorem ipsum and anything else that is supposed to be in a paragraph. Lorem ipsum and anything else that is supposed to be in a paragraph. Lorem ipsum and anything else that is supposed to be in a paragraph", stars: 1},
+    {title: "User2", desc: "Lorem ipsum and anything else that is supposed to be in a paragraph", stars: 2},
+    {title: "User3", desc: "Lorem ipsum and anything else that is supposed to be in a paragraph", stars: 3},
+    {title: "User4", desc: "Lorem ipsum and anything else that is supposed to be in a paragraph", stars: 4},
+    {title: "User5", desc: "Lorem ipsum and anything else that is supposed to be in a paragraph", stars: 5},
+    {title: "User1", desc: "Lorem ipsum and anything else that is supposed to be in a paragraph", stars: 1},
+    {title: "User2", desc: "Lorem ipsum and anything else that is supposed to be in a paragraph", stars: 2},
+    {title: "User3", desc: "Lorem ipsum and anything else that is supposed to be in a paragraph", stars: 3},
+    {title: "User4", desc: "Lorem ipsum and anything else that is supposed to be in a paragraph", stars: 4},
+    {title: "User5", desc: "Lorem ipsum and anything else that is supposed to be in a paragraph", stars: 5},
+  ]
 
   return (
     <div className={"w-full p-3 h-full flex flex-row items-start justify-start" + theme.primaryBg}>
@@ -27,8 +32,10 @@ function AppContainer() {
         </div>
       </div>
       <div className={"w-1/4 h-full p-1 md:p-2 md:pl-0 pl-0 flex flex-col" + theme.mainBg}>
-        <div className={"w-full h-full flex flex-col overflow-y-scroll justify-center border-2" + theme.mainBorder + theme.scrollbar}>
-          <div className={"text-center" + theme.mainText}>comentarios</div>
+        <div className={"w-full h-full flex flex-col overflow-y-scroll justify-start" + theme.scrollbar}>
+          {ph.map((props, idx) => {
+            return <CommentCard {...props} idx={idx} key={`comment-${idx}-on-this-sheet`}/>
+          })}
         </div>
       </div>
     </div>
@@ -36,3 +43,13 @@ function AppContainer() {
 }
 
 export default AppContainer
+
+/*
+  {userApp.map((row) => {
+    return (
+      <div className="w-full py-2 flex flex-row">
+        { row }
+      </div>
+    );
+  })}
+*/
