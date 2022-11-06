15104133d01eebe3f6464ad5d395b1c65d5934c7
YISR-UOH <yerko.sepulveda@pregrado.uoh.cl>
2022-11-06 05:16:11 -0300

    Update query.js

--- src/utils/query.js
+++ src/utils/query.js
@@ -1,23 +1,46 @@
 export async function spreadsheet(){
     
-    let id_user = localStorage.getItem('userid');
+  let id_user = localStorage.getItem('userid');
+
+  let result = await spreadsheet_query(id_user);
   
-    let result = await spreadsheet_query(id_user);
-    
-    return await result;
+  return await result;
+}
+
+
+async function spreadsheet_query(id_user){
+
+  var requestOptions = {
+    method: 'POST',
+    redirect: 'follow'
+  };
+  try{
+    const result = await fetch("https://vezzel-api.herokuapp.com/spreadsheet/"+id_user, requestOptions)
+    return await result.json();
+  }catch(e){
+    console.log(e);
   }
+}
+
+async function getUserComm(){
+  // retorna todos los comentarios de un usuario
+  let id_user = localStorage.getItem('userid');
+
+  let result = await getUserComm_query(id_user);
   
-  
-  async function spreadsheet_query(id_user){
-  
-    var requestOptions = {
-      method: 'POST',
-      redirect: 'follow'
-    };
-    try{
-      const result = await fetch("https://vezzel-api.herokuapp.com/spreadsheet/"+id_user, requestOptions)
-      return await result.json();
-    }catch(e){
-      console.log(e);
-    }
+  return await result;
+}
+
+async function getUserComm_query(id_user){
+
+  var requestOptions = {
+    method: 'POST',
+    redirect: 'follow'
+  };
+  try{
+    const result = await fetch("https://vezzel-api.herokuapp.com/getUserComm/"+id_user, requestOptions)
+    return await result.json();
+  }catch(e){
+    console.log(e);
   }
+}
