30e4cbee6800dcb1d57ed6bf38acdf8ca96316c2
YISR-UOH <yerko.sepulveda@pregrado.uoh.cl>
2022-11-19 23:27:41 -0300

    add querys chat

--- src/mensajes/utils/query.js
+++ src/mensajes/utils/query.js
@@ -1,4 +1,66 @@
 export async function getUserChats() {
     /* TO DO */
-    return Array();
+    let id_user = localStorage.getItem('userid');
+    let name_user = localStorage.getItem('username');
+
+    let result = await getUserChats_query(id_user, name_user);
+    return await result;
+}
+async function getUserChats_query(id_user, name_user){
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    
+    var raw = JSON.stringify({
+      "user1_id": id_user,
+      "user1_name": name_user
+    });
+    
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+    
+    try{
+        const result = await fetch("https://vezzel-api.herokuapp.com/getAllChat", requestOptions)
+        return await result.json();
+    }catch(e){
+        console.log(e);
+    }
+}
+
+export async function sendMsg(id_user2, name_user2, msg) {
+    /* TO DO */
+    let id_user = localStorage.getItem('userid');
+    let name_user = localStorage.getItem('username');
+
+    let result = await sendMsg_query(id_user, name_user,id_user2, name_user2, msg);
+    return await result;
+}
+async function sendMsg_query(id_user, name_user,id_user2, name_user2, msg){
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    
+    var raw = JSON.stringify({
+      "user1_id": id_user,
+      "user1_name": name_user,
+      "user2_id": id_user2,
+      "user2_name": name_user2,
+      "msg": msg
+    });
+    
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+    
+    try{
+        const result = await fetch("https://vezzel-api.herokuapp.com/saveChat", requestOptions)
+        return await result.json();
+    }catch(e){
+        console.log(e);
+    }
 }
