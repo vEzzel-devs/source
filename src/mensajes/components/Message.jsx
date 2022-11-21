acd1232c4f763eea1ae15cc96c7ae1ebb5679812
Vicente-G <vicentea.gonzalez@pregrado.uoh.cl>
2022-11-21 02:42:43 -0300

    msg hotfix

--- src/mensajes/components/Message.jsx
+++ src/mensajes/components/Message.jsx
@@ -4,13 +4,12 @@ import { ThemeContext } from "../../context/ThemeContext";
 function Message({content, side, time}) {
     const { theme } = useContext(ThemeContext);
     return (
-        <div className={"w-5/6 flex flex-row" + (side === "right" ?  "-reverse self-end" : " self-start")}>
+        <div className={"w-5/6 flex flex-row" + (side === "right" ?  " self-end justify-end" : " self-start justify-start")}>
+            {(side === "right") && time && <div className={"self-end px-2 text-xs" + theme.lightText}>{time}</div>}
             <div className={"p-1 px-2 text-md rounded-lg" + theme.lightBg}>
                 {content}
             </div>
-            {time && <div className={"self-end px-2 text-xs" + theme.lightText}>
-                {time}
-            </div>}
+            {(side === "left") && time && <div className={"self-end px-2 text-xs" + theme.lightText}>{time}</div>}
         </div>
     )
 }
